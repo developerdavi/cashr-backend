c042e3a275b61afa0f32d0db6550180a22cb6bb9
Davi Rodrigues <developer.davi@gmail.com>
2020-09-12 22:01:07 -0300

    feat: created user login route

--- models/user.ts
+++ models/user.ts
@@ -1,4 +1,6 @@
-import { Schema, model, Document } from 'mongoose';
+import { Schema, model, Document, Model } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import modelExists from '../helpers/modelExists';
 
 const schema = new Schema({
@@ -19,18 +21,72 @@ const schema = new Schema({
     type: String,
     required: true,
     default: 'user'
-  }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    },
+  }]
 }, {
   timestamps: true
 });
+
+schema.statics.findByCredentials = async (email: string, password: string) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatch) {
+    throw new Error('Invalid login credentials');
+  }
+
+  return user;
+};
+
+schema.methods.generateAuthToken = async function() {
+  const user = <IUser>this;
+
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_KEY);
+
+  user.tokens.push({ token });
+
+  await user.save();
+
+  return token;
+};
+
+schema.pre('save', async function (next) {
+  const user = <IUser>this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+interface IToken {
+  token: string;
+}
 
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
   role: string;
+  tokens?: Array<IToken>;
+  generateAuthToken(): Promise<string>;
 }
 
-const User = modelExists('users') ? model<IUser>('users') : model<IUser>('users', schema);
+export interface IUserModel extends Model<IUser> {
+  findByCredentials?(email: string, password: string): Promise<IUser>;
+}
+
+const User: IUserModel = modelExists('users') ? model<IUser>('users') : model<IUser>('users', schema);
 
 export default User;
---
+++ pages/api/users/login.ts
@@ -0,0 +1,46 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+import * as Yup from 'yup';
+import useCors from '../../../helpers/useCors';
+import User from '../../../models/user';
+import Database from '../../../services/mongodb';
+
+type body = {
+  email: string,
+  password: string
+}
+
+const userSchema = Yup.object().shape({
+  _id: Yup.string(),
+  email: Yup.string(),
+  name: Yup.string(),
+  createdAt: Yup.string(),
+  updatedAt: Yup.string()
+});
+
+export default async (req: NowRequest, res: NowResponse): Promise<void> => {
+  useCors(req, res);
+
+  const { email, password }: body = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({
+      error: 'Email and password fields are required'
+    });
+
+    return;
+  }
+
+  await Database.connect();
+
+  try {
+    const user = await User.findByCredentials(email, password);
+
+    const token = await user.generateAuthToken();
+
+    res.json({ user: userSchema.cast(user, { stripUnknown: true }), token });
+  } catch (error) {
+    res.status(400).json({
+      error: error.message
+    });
+  }
+};
