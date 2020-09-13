ba96b85169a54391ea7dfaaa48d2e3b83eda63ba
Davi Rodrigues <developer.davi@gmail.com>
2020-09-12 22:00:10 -0300

    feat: created cors helper

---
+++ helpers/useCors.ts
@@ -0,0 +1,18 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+import Cors from 'cors';
+
+type Options = {
+  methods?: Array<string>
+}
+
+export default function useCors(req: NowRequest, res: NowResponse, options: Options = {}): void {
+  const {
+    methods = ['GET', 'POST', 'PUT', 'DELETE']
+  } = options;
+
+  Cors({ methods })(req, res, (result) => {
+    if (result instanceof Error) {
+      throw result;
+    }
+  });
+}
