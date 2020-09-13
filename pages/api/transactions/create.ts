2461168d0d25f5d6e5d4241880adcc23312bb7dd
Davi Rodrigues <developer.davi@gmail.com>
2020-09-12 22:01:30 -0300

    feat: added cors everywhere

--- pages/api/transactions/create.ts
+++ pages/api/transactions/create.ts
@@ -1,8 +1,11 @@
 import { NowRequest, NowResponse } from '@vercel/node';
+import useCors from '../../../helpers/useCors';
 import Transaction from '../../../models/transaction';
 import Database from '../../../services/mongodb';
 
 export default async (req: NowRequest, res: NowResponse) : Promise<void> => {
+  useCors(req, res);
+
   await Database.connect();
 
   try {
--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,13 +1,16 @@
 import { NowRequest, NowResponse } from '@vercel/node';
+import useCors from '../../../helpers/useCors';
 import withAuth from '../../../middlewares/withAuth';
 import User from '../../../models/user';
 import Database from '../../../services/mongodb';
 
-const handler = async (req: NowRequest, res: NowResponse) : Promise<void> => {
+export default withAuth(async (req: NowRequest, res: NowResponse) : Promise<void> => {
   await Database.connect();
 
+  useCors(req, res);
+
   try {
-    const users = await User.find(req.query);
+    const users = await User.find(req.query, { tokens: false, __v: false, password: false });
 
     res.json(users);
   } catch (error) {
@@ -15,8 +18,6 @@ const handler = async (req: NowRequest, res: NowResponse) : Promise<void> => {
       error: error.message
     });
   }
-};
-
-export default withAuth(handler, {
+}, {
   admin: true
 });
